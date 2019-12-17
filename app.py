ddbcb9c62849bb96b70aebdb993c7fc1249e15fd
Eldervald <nikolay.ignatenko@outlook.com>
2019-12-18 00:35:48 +0300

    API fixes

--- app.py
+++ app.py
@@ -8,24 +8,30 @@ app = Flask(__name__)
 map_generator = MapGenerator()
 
 # debug
-# with open("static/data.json", "r", encoding="utf-8") as file_object:
-#     contents = json.loads(file_object.read())
-#     # hots = map_generator.calculate_map(contents)
-#     image = map_generator.calculate_map(contents)
-#     image.save('res/map.png')
-#     # print(json.dumps([{'coordinates': {'longitude': x, 'latitude': y}, 'weight': hot} for x, y, hot in hots]))
+with open("static/data.json", "r", encoding="utf-8") as file_object:
+    contents = json.loads(file_object.read())
+    # hots = map_generator.calculate_map(contents)
+    image = map_generator.calculate_map(contents)
+    image.save('res/map.png')
+    image.save("res/map.png")
+    with open("res/map.png", mode="rb") as file:
+        res_bytes = base64.b64encode(file.read())
+        res_string = res_bytes.decode('utf-8')
+    print(res_string)
+    # print(json.dumps([{'coordinates': {'longitude': x, 'latitude': y}, 'weight': hot} for x, y, hot in hots]))
 
 
 @app.route('/', methods=['POST'])
 def searcher_from_post():
     data = json.loads(request.data)
     # with open("static/data.json", "w", encoding="utf-8") as f:
-    #     json.dump(data, f)
+    #      json.dump(data, f)
     image = map_generator.calculate_map(data)
 
     image.save("res/map.png")
     with open("res/map.png", mode="rb") as file:
         res_string = base64.b64encode(file.read())
+    print(res_string)
     return json.dumps({'heatmapImageBase64': res_string})
 
     # post = [{'coordinates': {'longitude': x, 'latitude': y}, 'weight': hot} for x, y, hot in hots]
