bd7bad06a6dc88633864f15d23ad89c09f9f38b6
Eldervald <nikolay.ignatenko@outlook.com>
2019-12-18 00:36:59 +0300

    API fixes

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ app = Flask(__name__)
 map_generator = MapGenerator()
 
 # debug
-with open("static/data.json", "r", encoding="utf-8") as file_object:
-    contents = json.loads(file_object.read())
-    # hots = map_generator.calculate_map(contents)
-    image = map_generator.calculate_map(contents)
-    image.save('res/map.png')
-    image.save("res/map.png")
-    with open("res/map.png", mode="rb") as file:
-        res_bytes = base64.b64encode(file.read())
-        res_string = res_bytes.decode('utf-8')
-    print(res_string)
-    # print(json.dumps([{'coordinates': {'longitude': x, 'latitude': y}, 'weight': hot} for x, y, hot in hots]))
+# with open("static/data.json", "r", encoding="utf-8") as file_object:
+#     contents = json.loads(file_object.read())
+#     # hots = map_generator.calculate_map(contents)
+#     image = map_generator.calculate_map(contents)
+#     image.save('res/map.png')
+#
+#     with open("res/map.png", mode="rb") as file:
+#         res_bytes = base64.b64encode(file.read())
+#         res_string = res_bytes.decode('utf-8')
+#     print(res_string)
+#     # print(json.dumps([{'coordinates': {'longitude': x, 'latitude': y}, 'weight': hot} for x, y, hot in hots]))
 
 
 @app.route('/', methods=['POST'])
@@ -30,8 +30,8 @@ def searcher_from_post():
 
     image.save("res/map.png")
     with open("res/map.png", mode="rb") as file:
-        res_string = base64.b64encode(file.read())
-    print(res_string)
+        res_bytes = base64.b64encode(file.read())
+        res_string = res_bytes.decode('utf-8')
     return json.dumps({'heatmapImageBase64': res_string})
 
     # post = [{'coordinates': {'longitude': x, 'latitude': y}, 'weight': hot} for x, y, hot in hots]
